db6777920adeb6b7f394dc394ba24cb31677d503
SodoTeo <sodolescuteodor@gmail.com>
2021-10-29 00:33:06 +0300

    refactor: for @mui/material library use default imports of components
    refs: #4

--- src/ActionCard.js
+++ src/ActionCard.js
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import CardActionArea from '@mui/material/CardActionArea';
 
 const ActionCard = ({img, name, type}) =>  {
   return (
